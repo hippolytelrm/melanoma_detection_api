4a43015a4867ef1baf5fe1f90895c4b4565d6dae
hippolyte <hippochavic>
2021-09-21 22:16:47 +0200

    Updating model directory

--- melanoma_detection_api/model/model_loader.py
+++ melanoma_detection_api/model/model_loader.py
@@ -1,14 +1,28 @@
 import tensorflow as tf
 import traceback
 import sys
+import os
 
-def model_loader(model_name:str):
-    try:
-        model = tf.keras.models.load_model(model_name)
-        print("Model successfully loaded")
-    except:
-        print("Could not find the required model")
-        traceback.print_exc(file=sys.stdout)
+ALLOWED_EXTENSIONS = set(['h5', 'hdf5'])
+
+def model_loader(model_path:str):
+
+    EXTENSION = model_path.split('.')[-1]
+
+    if model_path.split('/')[-1] in os.listdir('model'):
+        if EXTENSION not in ALLOWED_EXTENSIONS:
+            raise ImportError('Wrong format')
+
+        try:
+            model = tf.keras.models.load_model(model_path)
+            print("Model successfully loaded")
+            return model
+        except:
+            pass
+
+    else:
+        raise ModuleNotFoundError('Model not found')
 
 
-    return model
+
+
